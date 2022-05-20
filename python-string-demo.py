7703d69dc5bb9b246126053a14cb71a2bb5c5bef
zhivkoststoyanov <zhivkostoyanovbg@gmail.com>
2022-05-20 13:25:03 +0300

    Initial commit of Python scrips demo

---
+++ python-string-demo.py
@@ -0,0 +1,7 @@
+#first_name = 'John'
+#last_name = 'Adams'
+
+first_name = input('Please enter your first name: ')
+last_name = input('Please enter your last name: ')
+
+print(first_name + last_name)
