fd474f71e3bd5a3b628fc1d68802f8ca3f2dad91
zhivkoststoyanov <zhivkostoyanovbg@gmail.com>
2022-05-26 13:41:34 +0300

    Initial commit of Python Loops demo

---
+++ python-loops.py
@@ -0,0 +1,24 @@
+# Loops
+
+# Loop through a collection
+# for name in ['John', 'Jay']:
+    # print(name)
+
+# Looping a number of times
+# for index in range(0,3):
+#     print(index)
+
+# Looping with a condition
+
+# names = ['John', 'Jay']
+# index = 0
+# while index < len(names):
+#     print(names[index])
+#     # Change the condition!!
+#     index = index + 1
+
+
+names = ['John', 'Jay']
+
+for name in names:
+    print(name)
